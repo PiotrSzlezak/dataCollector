d6d46d70c1c096e36f9d7566763ac14ef38069b4
Piotr Szlezak <piotr@szlezak.pl>
2021-01-29 14:58:25 +0100

    getLogs added to API

--- src/main/java/pl/ideopolis/dataCollector/logs/LogController.java
+++ src/main/java/pl/ideopolis/dataCollector/logs/LogController.java
@@ -1,25 +1,25 @@
 package pl.ideopolis.dataCollector.logs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/logs")
 public class LogController {
 
     private LogService logService;
 
     @Autowired
-    public LogController (LogService logService){
+    public LogController(LogService logService) {
         this.logService = logService;
     }
 
     @GetMapping("/getLogs")
-    public List<Log> getLogs (){
+    public List<Log> getLogs() {
         return logService.getLogs();
     }
 
